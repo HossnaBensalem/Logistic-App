bb9e226fafbf60904f2e20faae634d6e10160734
HossnaBensalem <hossnabensalem2@gmail.com>
2025-06-18 13:51:48 +0200

    import les bébliothéque

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
